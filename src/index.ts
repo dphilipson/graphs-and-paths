e3afa9992b81c7de03d9d9a7827ee39358a17484
David Philipson <david.philipson@gmail.com>
2016-12-16 18:19:44 -0800

    Make some initial type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-export default null;
+export type NodeId = number | string;
+
+
+export interface Graph {
+    nodes: Node[];
+    edges: Edge[];
+}
+
+export interface Node {
+    id: NodeId;
+    location: Point;
+}
+
+export interface Edge {
+    startNode: NodeId;
+    endNode: NodeId;
+    innerPoints?: Point[];
+}
+
+export interface Point {
+    x: number;
+    y: number;
+}
+
+export interface Destination {
+    edgeIndex: number;
+    distance: number;
+}
+
+export default function shortestPath(graph: Graph, start: Destination, end: Destination): NodeId[] {
+    return [];
+}
