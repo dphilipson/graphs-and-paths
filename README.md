98ddab0219cd0b5259b5802b958736ea7b4889a2
David Philipson <david.philipson@gmail.com>
2016-12-16 17:49:25 -0800

    Initial commit
    Set up basic npm tasks and config.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Broken Loop
+
+Find the shortest path from one point in a graph to another, including points on edges.
+
+Copyright © 2016 David Philipson
---
+++ test/test.ts
@@ -0,0 +1,9 @@
+import * as chai from "chai";
+
+const { expect } = chai;
+
+describe("tests", () => {
+    it("should run", () => {
+        // Just pass.
+    });
+});
